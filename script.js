b4796774ef491dc44714cef888f444d551627295
miguelvneto <miguelvieiraneto1@gmail.com>
2024-09-24 15:25:09 -0300

    Update JS
    nome das colunas minuscula
    troca de numero para Y e N, F e M

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// Colocando os dados no padrão utilizado pela API
-function padraoAPI(obj) {
-    // Mapeamento de valores
-    const valueMapping = {
-        "y": "2",
-        "m": "2",
-        "n": "1",
-        "f": "1"
-    };
-
-    // Colocando os dados no padrão utilizado pela API
-    const transformedObject = {};
-
-    for (const key in obj) {
-        let value = obj[key];
-
-        // Substituindo valores com base no mapeamento
-        transformedObject[key] = valueMapping[value] || value; // Mantém o valor original se não houver mapeamento
-
-        // Renomeando GENDER para MALE
-        if (key === "GENDER") {
-            transformedObject["MALE"] = transformedObject[key];
-            delete transformedObject[key]; // Remove a chave original
-        }
-    }
-
-    return transformedObject;
-}
-
 // Função para processar o envio do formulário
 function handleFormSubmit(event) {
     event.preventDefault(); // Evita o envio padrão do formulário
@@ -37,15 +8,12 @@ function handleFormSubmit(event) {
     // Converte o FormData em um objeto simples
     const dados = {};
     formData.forEach((value, key) => {
-        dados[key.toUpperCase()] = value;
+        dados[key] = value;
     });
 
-    // Converte para o padrão da requisição
-    const dadosPadronizados = padraoAPI(dados);
-
     // Converte o objeto para JSON
-    const jsonData = JSON.stringify(dadosPadronizados);
-
+    const jsonData = JSON.stringify(dados);
+    console.log(jsonData)
     // Faz a requisição POST para a URL
     fetch("http://127.0.0.1:5000/predict", {
         method: "POST",
